fda0652d759f0e61b40cdfa8c55651ef6287d960
aakriti1435 <write10aakriti07@gmail.com>
2021-06-02 12:58:40 +0530

    First View

--- musicController/settings.py
+++ musicController/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'handleApi.apps.HandleapiConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
