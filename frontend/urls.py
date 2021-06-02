fa1221b0544d7b2f11dba16e4217da19e313e86a
aakriti1435 <write10aakriti07@gmail.com>
2021-06-02 17:06:47 +0530

    Routes

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import index
 
 urlpatterns = [
-    path('', index , name='')
+    path('', index , name=''),
+    path('join/', index , name=''),
+    path('create/', index , name=''),
 ]
