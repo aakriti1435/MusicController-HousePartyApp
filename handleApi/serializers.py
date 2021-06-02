b7d7d0153dcf495da41cc11144fbd7b9a211923c
aakriti1435 <write10aakriti07@gmail.com>
2021-06-02 13:36:37 +0530

    Serializers

---
+++ handleApi/serializers.py
@@ -0,0 +1,10 @@
+# Serializers in Django REST Framework are responsible for converting objects into data types understandable by javascript and front-end frameworks.
+
+from rest_framework import serializers
+from .models import Room
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'code', 'host', 'guestCanPause', 'votesToSkip', 'createdAt')
--- handleApi/urls.py
+++ handleApi/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from .views import main
 
 urlpatterns = [
-    path('', main)
 ]
