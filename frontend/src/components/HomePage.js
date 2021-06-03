32468410a18f575f70da0d5c13adb1421e7c1ec3
aakriti1435 <write10aakriti07@gmail.com>
2021-06-03 13:54:42 +0530

    Home Page

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 export default class HomePage extends Component {
 	constructor(props) {
@@ -6,6 +8,28 @@ export default class HomePage extends Component {
 	}
 
 	render() {
-		return <h1>This is Home PAge</h1>;
+		return (
+			<Grid container spacing={3}>
+				<Grid item xs={12} align="center">
+					<Typography variant="h3" component="h3">
+						House Party
+					</Typography>
+				</Grid>
+				<Grid item xs={12} align="center">
+					<ButtonGroup
+						disableElevation
+						variant="contained"
+						color="primary"
+					>
+						<Button color="primary" to="/join" component={Link}>
+							Join A Room
+						</Button>
+						<Button color="secondary" to="/create" component={Link}>
+							Create A Room
+						</Button>
+					</ButtonGroup>
+				</Grid>
+			</Grid>
+		);
 	}
 }
