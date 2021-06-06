c0fe593da1053a5000fdaec665e392e21900abd8
aakriti1435 <write10aakriti07@gmail.com>
2021-06-06 18:46:36 +0530

    Play/Pause

--- spotifyAPI/views.py
+++ spotifyAPI/views.py
@@ -4,11 +4,7 @@ from rest_framework.views import APIView
 from requests import Request, post
 from rest_framework import status
 from rest_framework.response import Response
-from .utils import (
-    update_or_create_user_tokens,
-    is_spotify_authenticated,
-    execute_spotify_api_request
-)
+from .utils import *
 from handleApi.models import Room
 
 
@@ -108,5 +104,27 @@ class CurrentSong(APIView):
         }
 
         return Response(song, status=status.HTTP_200_OK)
+
+
+class PauseSong(APIView):
+    def put(self, response, format=None):
+        room_code = self.request.session.get('room_code')
+        room = Room.objects.filter(code=room_code)[0]
+        if self.request.session.session_key == room.host or room.guest_can_pause:
+            pause_song(room.host)
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        
+        return Response({}, status=status.HTTP_403_FORBIDDEN)
+    
+class PlaySong(APIView):
+    def put(self, response, format=None):
+        room_code = self.request.session.get('room_code')
+        room = Room.objects.filter(code=room_code)[0]
+        if self.request.session.session_key == room.host or room.guest_can_pause:
+            play_song(room.host)
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        
+        return Response({}, status=status.HTTP_403_FORBIDDEN)
+
 
 
