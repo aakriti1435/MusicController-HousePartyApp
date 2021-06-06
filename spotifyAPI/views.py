27fab2189321a8817f333331fd9e3498332002c7
aakriti1435 <write10aakriti07@gmail.com>
2021-06-06 13:24:46 +0530

    Authenticating

--- spotifyAPI/views.py
+++ spotifyAPI/views.py
@@ -21,4 +21,32 @@ class AuthURL(APIView):
         return Response({'url': url}, status=status.HTTP_200_OK)
 
 
+def spotify_callback(request, format=None):
+    code = request.GET.get('code')
+    error = request.GET.get('error')
+
+    response = post('https://accounts.spotify.com/api/token', data={
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': REDIRECT_URI,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET
+    }).json()
+
+    access_token = response.get('access_token')
+    token_type = response.get('token_type')
+    refresh_token = response.get('refresh_token')
+    expires_in = response.get('expires_in')
+    error = response.get('error')
+
+    if not request.session.exists(request.session.session_key):
+        request.session.create()
+
+    update_or_create_user_tokens(
+        request.session.session_key, 
+        access_token, token_type, 
+        expires_in, refresh_token
+    )
+
+    return redirect('frontend:')
 
