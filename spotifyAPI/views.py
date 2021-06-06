f47632c6cd311042bf9e79df72612bcb7342f3d6
aakriti1435 <write10aakriti07@gmail.com>
2021-06-06 13:57:34 +0530

    refreshTokens + isAuthenticated

--- spotifyAPI/views.py
+++ spotifyAPI/views.py
@@ -52,3 +52,13 @@ def spotify_callback(request, format=None):
 
     return redirect('frontend:')
 
+
+class IsAuthenticated(APIView):
+    def get(self, request, format=None):
+        is_authenticated = is_spotify_authenticated(
+            self.request.session.session_key)
+        return Response({'status': is_authenticated}, status=status.HTTP_200_OK)
+
+
+
+
